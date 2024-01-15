c4674c51269ec2905756c6f8950126e2564728e2
vishusharma1606 <78497868+vishusharma1606@users.noreply.github.com>
2024-01-15 23:14:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My First React app
+          My First Reactapp
         </p>
         <a
           className="App-link"
